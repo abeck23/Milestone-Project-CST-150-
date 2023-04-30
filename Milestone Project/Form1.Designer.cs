00edf7337cfe5f295f755eed0304b07d87bd8a27
Akikti <99299991+Akikti@users.noreply.github.com>
2023-04-30 18:47:41 -0400

    Version 1

---
+++ Milestone Project/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Milestone_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculate = new System.Windows.Forms.Button();
+            this.addInventory = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.order = new System.Windows.Forms.Button();
+            this.itemBox = new System.Windows.Forms.ComboBox();
+            this.descriptionBox = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.quantityBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.price = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(78, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Price:";
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(82, 249);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(95, 29);
+            this.calculate.TabIndex = 2;
+            this.calculate.Text = "Calculate";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // addInventory
+            // 
+            this.addInventory.Location = new System.Drawing.Point(184, 249);
+            this.addInventory.Name = "addInventory";
+            this.addInventory.Size = new System.Drawing.Size(95, 29);
+            this.addInventory.TabIndex = 3;
+            this.addInventory.Text = "Add Inventory";
+            this.addInventory.UseVisualStyleBackColor = true;
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(285, 249);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(95, 29);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "Clear";
+            this.clear.UseVisualStyleBackColor = true;
+            // 
+            // order
+            // 
+            this.order.Location = new System.Drawing.Point(386, 249);
+            this.order.Name = "order";
+            this.order.Size = new System.Drawing.Size(95, 29);
+            this.order.TabIndex = 5;
+            this.order.Text = "Order";
+            this.order.UseVisualStyleBackColor = true;
+            // 
+            // itemBox
+            // 
+            this.itemBox.FormattingEnabled = true;
+            this.itemBox.Items.AddRange(new object[] {
+            "G2 Pilot",
+            "X-Treme Roller Ball",
+            "Skillcraft Ball Point"});
+            this.itemBox.Location = new System.Drawing.Point(142, 74);
+            this.itemBox.Name = "itemBox";
+            this.itemBox.Size = new System.Drawing.Size(137, 21);
+            this.itemBox.TabIndex = 6;
+            this.itemBox.SelectedIndexChanged += new System.EventHandler(this.itemBox_SelectedIndexChanged);
+            // 
+            // descriptionBox
+            // 
+            this.descriptionBox.Location = new System.Drawing.Point(335, 74);
+            this.descriptionBox.Name = "descriptionBox";
+            this.descriptionBox.Size = new System.Drawing.Size(280, 122);
+            this.descriptionBox.TabIndex = 9;
+            this.descriptionBox.Text = "";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(78, 113);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "QTY:";
+            // 
+            // quantityBox
+            // 
+            this.quantityBox.Location = new System.Drawing.Point(148, 113);
+            this.quantityBox.Name = "quantityBox";
+            this.quantityBox.Size = new System.Drawing.Size(100, 20);
+            this.quantityBox.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(422, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 24);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Description";
+            // 
+            // price
+            // 
+            this.price.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.price.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.price.Location = new System.Drawing.Point(148, 158);
+            this.price.Name = "price";
+            this.price.Size = new System.Drawing.Size(100, 23);
+            this.price.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 347);
+            this.Controls.Add(this.price);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.quantityBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.descriptionBox);
+            this.Controls.Add(this.itemBox);
+            this.Controls.Add(this.order);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.addInventory);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pen Supplier";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Button addInventory;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button order;
+        private System.Windows.Forms.ComboBox itemBox;
+        private System.Windows.Forms.RichTextBox descriptionBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox quantityBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label price;
+    }
+}
+
